95562c024c1b44244659f6e3845a626e42cc2ffd
Nathan Hawkins <utsl@utsl.org>
2023-02-06 18:31:14 +0000

    add some more modules

--- opensips/default.nix
+++ opensips/default.nix
@@ -1,7 +1,36 @@
 { lib, stdenv, pkgs, fetchFromGitHub }:
 
 let
-  extra_modules = "db_postgres httpd json presence presence_dialoginfo pua pua_dialoginfo rls tls_openssl db_mysql db_sqlite pi_http presence_dfks presence_mwi presence_xml proto_tls pua_bla pua_mi pua_usrloc pua_xmpp tls_mgm xcap_client xcap";
+  extra_modules = (builtins.concatStringsSep " " [
+    "db_postgres"
+    "httpd"
+    "json"
+    "presence"
+    "presence_dialoginfo"
+    "pua"
+    "pua_dialoginfo"
+    "rls"
+    "tls_openssl"
+    "db_mysql"
+    "db_sqlite"
+    "pi_http"
+    "presence_dfks"
+    "presence_mwi"
+    "presence_xml"
+    "proto_tls"
+    "pua_bla"
+    "pua_mi"
+    "pua_usrloc"
+    "pua_xmpp"
+    "tls_mgm"
+    "xcap_client"
+    "xcap"
+    "event_kafka"
+    "cachedb_redis"
+    "carrierroute"
+    "uuid"
+    "rest_client"
+  ]);
 in
 stdenv.mkDerivation rec {
   pname = "opensips";
@@ -15,7 +44,7 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = with pkgs; [ bison flex which pkg-config ];
-  buildInputs = with pkgs; [ ncurses expat openssl postgresql libxml2 libconfuse json_c libmicrohttpd_0_9_72 mysql80 zstd sqlite curl ];
+  buildInputs = with pkgs; [ ncurses expat openssl postgresql libxml2 libconfuse json_c libmicrohttpd_0_9_72 mysql80 zstd sqlite curl rdkafka hiredis libconfuse libuuid ];
 
   patchPhase = ''
     # these Makefiles don't use xml-config, so they don't find libxml2's include files. RLS module does, so I copy it, and change the
