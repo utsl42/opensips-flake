a4354002e9be170777997179cd7614dc4495db70
Nathan Hawkins <utsl@utsl.org>
2024-03-26 14:32:08 +0000

    update to base on 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "OpenSIPS - flexible and robust SIP (RFC3261) server";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
   };
 
   outputs = { self, nixpkgs }:
